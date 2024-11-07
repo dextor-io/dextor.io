7019ce24929a08d55bd11cde6298922de64d4ddd
ibrahimjogilkar <jogilkaribrahim@gmail.com>
2024-11-07 22:23:18 +0530

    Improved UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import logo from '../assets/dextor-white.svg';
 
 function Navbar() {
--- src/components/ui/Pagination.jsx
+++ src/components/ui/Pagination.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 
-const Pagination = ({ projectsPerPage, totalProjects, paginate, currentPage }) => {
-  const pageNumbers = [];
+const Pagination = ({projectsPerPage, totalProjects, paginate, currentPage}) => {
+    const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalProjects / projectsPerPage); i++) {
-    pageNumbers.push(i);
-  }
+    for (let i = 1; i <= Math.ceil(totalProjects / projectsPerPage); i++) {
+        pageNumbers.push(i);
+    }
 
-  return (
-    <nav className="flex justify-center mt-8">
-      <ul className="flex space-x-2">
-        {pageNumbers.map(number => (
-          <li key={number}>
-            <button
-              onClick={() => paginate(number)}
-              className={`px-3 py-1 rounded ${
-                currentPage === number
-                  ? 'bg-primary-500 text-black'
-                  : 'bg-gray-200 text-gray-700 hover:bg-gray-300'
-              }`}
-            >
-              {number}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="flex justify-center mt-8">
+            <ul className="flex space-x-2">
+                {pageNumbers.map(number => (
+                    <li key={number}>
+                        <button
+                            onClick={() => paginate(number)}
+                            className={`px-3 py-1 rounded ${
+                                currentPage === number
+                                    ? 'bg-primary-500 text-black'
+                                    : 'bg-gray-200 text-gray-700 hover:bg-gray-300'
+                            }`}
+                        >
+                            {number}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 };
 
 export default Pagination;
