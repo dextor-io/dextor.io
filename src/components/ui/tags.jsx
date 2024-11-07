65d605c37fb7adb2ad1eefb57068527def6eaaa6
ibrahimjogilkar <ibrahim.jogilkari@ilmtec.in>
2024-11-07 21:12:26 +0530

    Improved color

---
+++ src/components/ui/tags.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TechTags({techList}) {
+    const colorPairs = [
+        {background: 'bg-pink-200', text: 'text-pink-800'},
+        {background: 'bg-yellow-200', text: 'text-yellow-800'},
+        {background: 'bg-green-200', text: 'text-green-800'},
+        {background: 'bg-blue-200', text: 'text-blue-800'},
+        {background: 'bg-purple-200', text: 'text-purple-800'},
+        {background: 'bg-teal-200', text: 'text-teal-800'},
+        {background: 'bg-orange-200', text: 'text-orange-800'},
+    ];
+    return (
+        <div className="flex flex-wrap gap-2">
+            {techList.map((tech) => {
+                const {background, text} = colorPairs[Math.floor(Math.random() * colorPairs.length)];
+
+                return (
+                    <span
+                        key={tech}
+                        className={`px-2 py-1 text-sm ${text} ${background} rounded-md`}
+                    >
+            {tech}
+          </span>
+                );
+            })}
+        </div>
+    );
+}
+
+// Define propTypes for the component
+TechTags.propTypes = {
+    techList: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default TechTags;
