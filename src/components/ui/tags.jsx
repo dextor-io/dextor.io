d5a8620a7b5bd3de83069c099b6ef911fb572f12
ibrahimjogilkar <jogilkaribrahim@gmail.com>
2024-11-07 22:22:20 +0530

    Improved UI

--- src/components/ui/tags.jsx
+++ src/components/ui/tags.jsx
@@ -2,20 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function TechTags({techList}) {
-    const colorPairs = [
-        {background: 'bg-pink-200', text: 'text-pink-800'},
-        {background: 'bg-yellow-200', text: 'text-yellow-800'},
-        {background: 'bg-green-200', text: 'text-green-800'},
-        {background: 'bg-primary-200', text: 'text-primary-800'},
-        {background: 'bg-purple-200', text: 'text-purple-800'},
-        {background: 'bg-teal-200', text: 'text-teal-800'},
-        {background: 'bg-orange-200', text: 'text-orange-800'},
-    ];
     return (
         <div className="flex flex-wrap gap-2">
             {techList.map((tech) => {
-                const {background, text} = colorPairs[Math.floor(Math.random() * colorPairs.length)];
-
                 return (
                     <span
                         key={tech}
